587f01ca6c196a8849b91ca987a3a91e05cbaebe
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-28 07:24:55 +0530

    Added : code for appending values

--- test/java/com/bridgelabz/MyNodeTest/MyLinkedListTest.java
+++ test/java/com/bridgelabz/MyNodeTest/MyLinkedListTest.java
@@ -5,6 +5,17 @@ import org.junit.Test;
 
 public class MyLinkedListTest {
 	@Test
+	public void givenThreeValues_WhenLinked_ShouldPassLinkedListTest() {
+		MyNode<Integer> myFirstNode = new MyNode<>(56);	
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		myFirstNode.setNext(mySecondNode);
+		mySecondNode.setNext(myThirdNode);
+		boolean result = myFirstNode.getNext().equals(mySecondNode) && 
+						 mySecondNode.getNext().equals(myThirdNode);
+		Assert.assertTrue(result);	
+	}
+	@Test
 	public void givenThreeValues_WhenLinked_ShouldBeAddedToTop() {
 		MyNode<Integer> myFirstNode = new MyNode<>(70);	
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
@@ -17,6 +28,21 @@ public class MyLinkedListTest {
 		boolean result = myLinkedList.head.equals(myThirdNode) &&
 						 myLinkedList.head.getNext().equals(mySecondNode) &&
 						 myLinkedList.tail.equals(myFirstNode);
+		Assert.assertTrue(result);
+	}
+	@Test
+	public void givenThreeValues_WhenLinked_ShouldBeAddedToLast() {
+		MyNode<Integer> myFirstNode = new MyNode<>(70);	
+		MyNode<Integer> mySecondNode = new MyNode<>(30);
+		MyNode<Integer> myThirdNode = new MyNode<>(56);
+		MyLinkedList myLinkedList = new MyLinkedList();
+		myLinkedList.append(myFirstNode);
+		myLinkedList.append(mySecondNode);
+		myLinkedList.append(myThirdNode);
+		myLinkedList.printNodes();
+		boolean result = myLinkedList.tail.equals(myThirdNode) &&
+				 myLinkedList.head.getNext().equals(mySecondNode) &&
+				 myLinkedList.head.equals(myFirstNode);
 		Assert.assertTrue(result);
 	}
 }
