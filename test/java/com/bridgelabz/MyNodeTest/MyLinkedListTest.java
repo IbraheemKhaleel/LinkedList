b0400b45422727eb1c43fd6b9d466f932139087d
IbraheemKhaleel <ikhaleelem@gmail.com>
2020-10-28 08:40:55 +0530

    Added : code for testing delete operation

--- test/java/com/bridgelabz/MyNodeTest/MyLinkedListTest.java
+++ test/java/com/bridgelabz/MyNodeTest/MyLinkedListTest.java
@@ -8,7 +8,7 @@ public class MyLinkedListTest {
 	public void givenThreeValues_WhenLinked_ShouldPassLinkedListTest() {
 		MyNode<Integer> myFirstNode = new MyNode<>(56);	
 		MyNode<Integer> mySecondNode = new MyNode<>(30);
-		MyNode<Integer> myThirdNode = new MyNode<>(70);
+		MyNode<Integer> myThirdNode =  new MyNode<>(70);
 		myFirstNode.setNext(mySecondNode);
 		mySecondNode.setNext(myThirdNode);
 		boolean result = myFirstNode.getNext().equals(mySecondNode) && 
